61a25c501c0a1b0f85ddb34706cd1b7c86ad3db5
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-06-13 23:11:01 +0900

    1st Commit

---
+++ src/components/pages/join/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Join from '../../oganisms/Join';
+
+const join = () => {
+  return (
+    <div>
+      <div>회원가입 페이지</div>
+      <div>
+        <Join />
+      </div>
+    </div>
+  );
+};
+
+export default join;
